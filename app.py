ba8765baf35f91265ded3c649439be6147072a4b
Joe Rosa <joerosa@joes-air.myfiosgateway.com>
2024-08-09 21:14:25 -0400

    updated to support cors

--- app.py
+++ app.py
@@ -51,20 +51,20 @@ def calendar():
 # API route to handle chatbot requests
 @app.route('/api/employee_assistant', methods=['POST'])
 def employee_assistant():
-    try:
-        external_api_url = "https://demo.airia.com/platform/api/PipelineExecution/get_bitcoin_price"
-        headers = {
-            "X-API-Key": "d465b2d3-4b4c-4167-83ee-e7c144664b35",
-            "Content-Type": "application/json"
-        }
-        response = requests.post(external_api_url, headers=headers, json=request.json)
+    external_api_url = "https://demo.airia.com/platform/api/PipelineExecution/employee_assistant"
+    
+    headers = {
+        "X-API-Key": "d465b2d3-4b4c-4167-83ee-e7c144664b35",
+        "Content-Type": "application/json"
+    }
 
+    try:
+        response = requests.post(external_api_url, headers=headers, json=request.json)
         if response.status_code == 200:
             return jsonify(response.json())
         else:
             error_message = response.json().get('message', 'Unknown error occurred')
-            return jsonify({"error": error_message}), response.status_code
-
+            return jsonify({"error": error_message, "status_code": response.status_code}), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
     
